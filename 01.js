322ef889dbc32f380b26f803d679be825304e989
syedusmanhabib <126954761+syedusmanhabib@users.noreply.github.com>
2024-07-22 16:05:34 +0000

    jgfg

---
+++ 01.js
@@ -0,0 +1,27 @@
+const accountID = 123444
+let accountEmail ="usmanhabib726@gmail.com"
+var accountPassword = "jgfyg8264"
+accountCity = "Karachi"
+
+// accountID = 5
+
+accountEmail = "hduduk@gmail.com"
+accountPassword ="766763"
+accountCity = "peterburg"
+
+console.log(accountID);
+
+console.table([accountEmail, accountID, accountPassword, accountCity])
+
+
+// data types 
+// number = 2
+// String = "usman"
+// Boolean = true/false
+// null = standard value
+// Symbol = unique
+// undefined=>
+//     bigint = 
+
+
+
